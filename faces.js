277d02dc9d38e86b0afb5c9f72d1430c8c60c137
wordsandfaces <wordsandfaces@ya.ru>
2026-03-29 19:33:57 +0300

    Add files via upload

---
+++ faces.js
@@ -0,0 +1,28 @@
+window.FACES_DATA = [
+  {
+    name: "Данила Гульков",
+    gender: "мужской",
+    age: 25,
+    city: "Москва",
+    photo: "https://trailers.filmtoolz.ru/images/photos_people/hq/1010929.jpg",
+    education: "ГИТИС",
+    hairColor: "Русый",
+    hairLength: "Средняя длина",
+    eyeColor: "Серо-голубой",
+    height: 180,
+    bodyType: "Стройное",
+    skills: ["Джаз-модерн", "Балет", "Вальс", "Брейк-данс", "Гопак", "Джаз", "Футбол", "Хоккей", "Теннис настольный", "Бильярд", "Бадминтон", "Бокс", "Акробатика", "Шашки", "Шахматы", "Хоккей с мячом (бенди)", "Мини-футбол", "Плавание", "Барабанная установка", "Бас-гитара", "Аккордеон", "Бас-балалайка", "Бубен", "Балалайка", "Барабан", "Акустическая гитара", "Варган", "Мюзикл", "Народный", "Рок", "Микс-вокал", "Баритон", "Рэп", "Битбокс", "Панк-рок", "Оперный", "Общетеатральное пение"],
+    languages: ["Русский", "Английский", "Французский"],
+    filmography: ["Тайна за углом", "Аллигатор", "Колбаса", "Золото скифов"],
+    theatre: ["Московский драматический театр им. М.Н. Ермоловой (Театр-центр им. Ермоловой)", "Вне…театр"],
+    gallery: [
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1010929.jpg",
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1049761.jpg",
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1049760.jpg",
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1010930.jpg",
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1010931.jpg"
+    ],
+    video: "https://prefs.filmtoolz.ru/files/u50220/video/2628467/2628467_hd720p.mp4",
+    filmtoolz: "https://casting.filmtoolz.ru/3309938/"
+  }
+];
