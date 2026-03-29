7e850448864c0043536e23a51238cb10a4b4c8fa
wordsandfaces <wordsandfaces@ya.ru>
2026-03-29 21:55:33 +0300

    Add files via upload

--- faces.js
+++ faces.js
@@ -5,7 +5,7 @@ window.FACES_DATA = [
     age: 25,
     city: "Москва",
     photo: "https://trailers.filmtoolz.ru/images/photos_people/hq/1010929.jpg",
-    education: "ГИТИС",
+    education: "ГИТИС, 2022, Мастерская О.Е. Меньшикова",
     hairColor: "Русый",
     hairLength: "Средняя длина",
     eyeColor: "Серо-голубой",
@@ -24,5 +24,31 @@ window.FACES_DATA = [
     ],
     video: "https://prefs.filmtoolz.ru/files/u50220/video/2628467/2628467_hd720p.mp4",
     filmtoolz: "https://casting.filmtoolz.ru/3309938/"
+  },
+  {
+    name: "Богдан Карчевский",
+    gender: "мужской",
+    age: 19,
+    city: "Москва",
+    photo: "https://trailers.filmtoolz.ru/images/photos_people/hq/1076748.jpg",
+    education: "ВТУ им. М.С. Щепкина, Мастерская В.А. Сафронова",
+    hairColor: "Русый",
+    hairLength: "Средняя длина",
+    eyeColor: "Серо-голубой",
+    height: 180,
+    bodyType: "Атлетичное",
+    skills: ["Русские танцы", "Общетеатральные танцы", "Крыжачок", "Народные танцы", "Полонез", "Конный спорт", "Волейбол", "Футбол", "Баскетбол", "Карате", "Варган", "Блок-флейта", "Бас-балалайка", "Укулеле", "Гитара", "Академический вокал"],
+    languages: ["Русский", "Английский", "Испанский"],
+    filmography: [],
+    theatre: ["Тамбовский государственный драматический театр"],
+    gallery: [
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1076748.jpg",
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1076757.jpg",
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1076763.jpg",
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1076758.jpg",
+      "https://trailers.filmtoolz.ru/images/photos_people/hq/1076755.jpg"
+    ],
+    video: "https://prefs.filmtoolz.ru/files/u87833/video/2764033/2764033_hd720p.mp4",
+    filmtoolz: "https://casting.filmtoolz.ru/4108095/"
   }
 ];
